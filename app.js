7753525d105f1050b29f67acc70fa9d853b14c7c
Saurabh Dang <saurabh.dang@technoscore.net>
2023-06-25 12:25:52 +0530

    object type

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-var num1 = 5;
-var num2 = 2.8;
-var printResult = true;
-var phrase = 'Result is: ';
-add(num1, num2, printResult, phrase);
+var person = {
+    name: "saurabh",
+    age: 34
+};
+console.log(person.name);
---
+++ app.ts
@@ -0,0 +1,14 @@
+const person1: {
+    name: string;
+    age: number;
+} = {
+    name: "saurabh",
+    age: 34
+}
+
+const person = {
+    name: "saurabh",
+    age: 34
+}
+
+console.log(person.name);
